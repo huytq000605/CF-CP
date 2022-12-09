d8eb28c6367086cfbe77f2f20fe1dd5e04752163
huy.tran <huytq000605@gmail.com>
2022-12-09 16:31:33 +0700

    AOC 2022 Day 9

--- Advent of Code 2022/9_1.py
+++ Advent of Code 2022/9_1.py
@@ -20,34 +20,34 @@ class BreakOutException(Exception):
 def main():
     lines = sys.stdin.read().splitlines()
     m, n = 2000, 2000
-    points = [(1000, 1000) for i in range(10)]
+    T = (1000, 1000)
+    H = (1000, 1000)
     ds = {"L": (0, -1), "R": (0, 1), "U": (-1, 0), "D": (1, 0)}
     seen = set([(1000, 1000)])
     for line in lines:
         d, steps = line.split()
+        print(line)
         steps = int(steps)
         for _ in range(steps):
             dr, dc = ds[d]
-            points[0] = (points[0][0] + dr, points[0][1] + dc)
-            for i in range(9):
-                H, T = points[i], points[i+1]
-                distance = max(abs(T[0] - H[0]), abs(T[1] - H[1]))
-                if distance >= 2:
-                    dr, dc = 0, 0
-                    if T[0] < H[0]:
-                        dr = 1
-                    elif T[0] > H[0]:
-                        dr = -1
+            H = (H[0] + dr, H[1] + dc)
+            print("H", H)
+            distance = max(abs(T[0] - H[0]), abs(T[1] - H[1]))
+            if distance >= 2:
+                dr, dc = 0, 0
+                if T[0] < H[0]:
+                    dr = 1
+                elif T[0] > H[0]:
+                    dr = -1
 
-                    if T[1] < H[1]:
-                        dc = 1
-                    elif T[1] > H[1]:
-                        dc = -1
+                if T[1] < H[1]:
+                    dc = 1
+                elif T[1] > H[1]:
+                    dc = -1
 
-                    T = (T[0] + dr, T[1] + dc)
-                points[i+1] = T
-                if i == 8:
-                    seen.add(points[-1])
+                T = (T[0] + dr, T[1] + dc)
+                print("T", T)
+                seen.add(T)
     print(len(seen))
 
 
