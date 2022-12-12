52109d258161c9a1034dae520313ed78e8e5c77c
huy.tran <huytq000605@gmail.com>
2022-12-12 14:16:11 +0700

    AOC 2022 Day 11

--- Advent of Code 2022/11_1.py
+++ Advent of Code 2022/11_1.py
@@ -31,10 +31,7 @@ def main():
         i += 1
     inspected = [0 for i in range(8)]
     testcases = [(13, 1, 7), (7, 3, 6), (3, 5, 4), (19, 2, 6), (5, 0, 5), (2, 7, 0), (11, 2, 4), (17, 1, 3)]
-    div = 1
-    for tc in testcases:
-        div *= tc[0]
-    
+
     def test(item, monkey):
         nonlocal testcases, monkeys
         div, t, f = testcases[monkey]
@@ -43,12 +40,10 @@ def main():
         else:
             monkeys[f].append(item)
 
-    turns = [0 for i in range(8)]
-    for turn in range(10000):
+    for turn in range(20):
         for monkey in range(8):
             items = monkeys[monkey]
             inspected[monkey] += len(items)
-            turns[monkey] = len(items)
             for item in items:
                 if monkey == 0:
                     item *= 11
@@ -66,12 +61,10 @@ def main():
                     item *= 7
                 elif monkey == 7:
                     item += 8
-                item %= div
+                item //= 3
                 test(item, monkey)
             monkeys[monkey] = []
-    inspected.sort()
     print(inspected)
-    print(inspected[-1] * inspected[-2])
 
 
 if __name__ == "__main__":
