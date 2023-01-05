925cf70a4d613a105e6129a1087cded66d96d41f
huytq <huytq000605@gmail.com>
2023-01-05 23:19:56 +0800

    c

--- cf.go
+++ cf.go
@@ -13,24 +13,6 @@ var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
 func print(a ...interface{})            { fmt.Println(a...) }
 func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f+"\n", a...) }
-
-func main() {
-	defer writer.Flush()
-
-	var testcases int64
-	scanf("%d", &testcases)
-  var i int64
-	for i = 0; i < testcases; i++ {
-		var n, x, y int64
-		scanf("%d %d %d", &n, &x, &y)
-    var a, b string
-    scanf("%s", &a)
-    scanf("%s", &b)
-
-		print(solve(n, x, y, a, b))
-	}
-}
-
 func max(nums ...int64) int64 {
   result := nums[0]
 	for _, num := range nums {
@@ -51,63 +33,32 @@ func min(nums ...int64) int64 {
   return result
 }
 
-func solve(n, x, y int64, a, b string) int64 {
-  diffs := make([]int64, 0, n)
+func main() {
+	defer writer.Flush()
+
+	var testcases int64
+	scanf("%d", &testcases)
   var i int64
-  for i = 0; i < n; i++ {
-    if a[i] != b[i] {
-      diffs = append(diffs, i)
+	for i = 0; i < testcases; i++ {
+		var n, k int64
+		scanf("%d %d", &n, &k)
+    graph := make([][]int64, n)
+    var j int 64
+    for j = 2; j < n+1; j++ {
+      graph[j] = make([]int, 0)
+      var p int64
+      scanf("%d", &p)
+      graph[p-1] = append(graph[p-1], j)
     }
-  }
 
-  m := int64(len(diffs))
+		print(solve(n, k, graph))
+	}
+}
 
-  if m % 2 == 1 {
-    return -1
-  }
+func dfs(u) {
 
-  if x >= y {
-    if m == 2 && diffs[0] + 1 == diffs[1] {
-      return min(x, y*2)
-    }
-    return y * m / 2
-  }
+}
 
-  dp := make([][]int64, m)
-  for i = 0; i < m; i++ {
-    dp[i] = make([]int64, m)
-    var j int64
-    for j = 0; j < m; j++ {
-      dp[i][j] = -1
-    }
-  }
-  var dfs func(i, drop int64) int64
-  dfs = func(i, drop int64) int64 {
-    if i >= m {
-      if drop == 0 {
-        return 0
-      }
-      return math.MaxInt64
-    }
-    if dp[i][drop] != -1 {
-      return dp[i][drop]
-    }
-    result := dfs(i+1, drop+1)
-    if drop > 0 {
-      this := dfs(i+1, drop-1)
-      if this != math.MaxInt64 {
-        result = min(result, this + y)
-      }
-    }
-    if i < m-1 {
-      this := dfs(i+2, drop)
-      if this != math.MaxInt64 {
-        result = min(result, this + min(y, x * (diffs[i+1] - diffs[i])))
-      }
-    }
-    dp[i][drop] = result
-    return result
-  }
-  res := dfs(0, 0)
-  return res
+func solve(n, k int64, graph [][]int64) int64 {
+
 }
--- cf.py
+++ cf.py
@@ -2,63 +2,76 @@ from functools import lru_cache as cache
 from collections import Counter, defaultdict, deque
 import math
 import bisect
+import string
 from heapq import *
 # from sortedcontainers import SortedSet, SortedList, SortedDict
-
+ 
 import sys
 def get_int(): return int(input())
 def get_ints(): return map(int, sys.stdin.readline().strip().split())
 def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
 def get_string(): return sys.stdin.readline().strip()
-
+ 
 def main():
-    testcases = get_int()
-    for tc in range(testcases):
-        n, k = get_ints()
-        a = get_list()
-        k = k-1
-        print(solve(a, n, k))
+    n = get_int()
+    a = get_list()
+    q = get_int()
+    queries = []
+    for i in range(q):
+        p, x = get_ints()
+        queries.append((p, -x, i))
+    queries.sort()
+    solve(n, a, queries)
+
+def solve(n, a, queries):
+    dp = [0 for i in range(n)]
+    dp[0] = 1
+    prefix = [0 for i in range(n)]
+    suffix = [0 for i in range(n+5)]
+    for i, x in enumerate(a):
+        dp[i] = min(dp[i-1] + 1, x)
+        if i > 0:
+            prefix[i] = prefix[i-1]
+        prefix[i] += dp[i]
+    for i in range(n-1, -1, -1):
+        if i < n-1:
+            suffix[i] = suffix[i+1]
+        suffix[i] += dp[i]
+    print(dp)
+    print(suffix)
+    result = [0 for i in range(len(queries))]
+    lowest_idx_not_affected = 0
+    for p, x, i  in queries:
+        x = -x
+        res = 0
+        p = p-1
+        # find lowest index (>p) satisfy new_dp[i] 
+        lowest_idx_not_affected = max(lowest_idx_not_affected, p + 1)
+        new_dp = x
+        if p > 0:
+            res += prefix[p-1]
+            new_dp = min(new_dp, dp[p-1]+1)
+        new_dp_p = new_dp
+        while lowest_idx_not_affected < n:
+            new_dp = min(a[lowest_idx_not_affected], new_dp + 1) 
+            if new_dp >= a[lowest_idx_not_affected]:
+                break
+            lowest_idx_not_affected += 1
+        print(a, p, x, lowest_idx_not_affected)
 
 
-def solve(a, n, k):
-    left, right = deque(), deque()
-    cur, worst = 0, 0
-    for i in range(k-1, -1, -1):
-        cur += a[i]
-        worst = min(worst, cur)
-        if cur >= 0:
-            left.appendleft((cur, worst))
-            cur, worst = 0, 0
-    left.appendleft((cur, worst))
+        no_of_num = lowest_idx_not_affected - p
+        print(no_of_num)
+        print(new_dp_p)
+        print(res)
+        res += no_of_num * (no_of_num - 1) // 2
+        res += new_dp_p * no_of_num
+        res += suffix[lowest_idx_not_affected]
+        result[i] = res
 
-    cur, worst = 0, 0
-    for i in range(k+1, n):
-        cur += a[i]
-        worst = min(worst, cur)
-        if cur >= 0:
-            right.append((cur, worst))
-            cur, worst = 0, 0
-    right.append((cur, worst))
+    for res in result:
+        print(res)
 
-    cur = a[k]
-    while left and right:
-        acted = False
-        if -left[-1][1] <= cur:
-            acted = True
-            cur += left.pop()[0]
-
-        if -right[0][1] <= cur:
-            acted = True
-            cur += right.popleft()[0]
-
-        if not acted:
-            return "NO"
-
-
-    
-
-    return "YES"
 
 if __name__ == "__main__":
     main()
-
