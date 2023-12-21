ec5b804624acf03e3515a11d9159653fa05649ae
huytq <huytq000605@gmail.com>
2023-12-21 22:51:20 +0700

    AOC 2023 Day 21 Part 1

--- Advent of Code 2023/21_1.py
+++ Advent of Code 2023/21_1.py
@@ -20,56 +20,34 @@ def get_string(): return sys.stdin.readline().strip()
 class BreakOutException(Exception):
     pass
 
-def f(n):
-    a0 = 3849
-    a1 = 34318
-    a2 = 95133
-
-    b0 = a0
-    b1 = a1-a0
-    b2 = a2-a1
-    return b0 + b1*n + (n*(n-1)//2)*(b2-b1)
-
 
 def main():
     lines = sys.stdin.read().splitlines()
     grid = [[c for c in line] for line in lines]
     m, n = len(grid), len(grid[0])
     ds = [(0,1),(1,0),(-1,0),(0,-1)]
-    s = set()
+    dq = []
     for r in range(m):
         for c in range(n):
             if grid[r][c] == "S":
                 grid[r][c] = "."
-                s.add((r, c, 0, 0))
+                dq.append((r, c))
                 break
-    for _ in range(65):
-        ns = set()
-        for tr, tc, r, c in s:
+    dq = deque(dq)
+    for _ in range(64):
+        ndq = set()
+        while dq:
+            r, c = dq.popleft()
             for dr, dc in ds:
                 nr, nc = r + dr, c + dc
-                ntr, ntc = tr, tc
-                if nr < 0:
-                    nr += m
-                    ntr = tr - 1
-                if nc < 0:
-                    nc += n
-                    ntc = tc - 1
-                if nr >= m:
-                    nr -= m
-                    ntr = tr + 1
-                if nc >= n:
-                    nc -= n
-                    ntc = tc + 1
-                if grid[nr][nc] == ".":
-                    ns.add((ntr, ntc, nr, nc))
-
-        s = ns
-    print(len(s))
+                if nr < 0 or nr >= m or nc < 0 or nc >= n:
+                    continue
+                if grid[nr][nc] == ".": 
+                    ndq.add((nr, nc))
+        dq = deque(ndq)
+    print(len(dq))
             
 
 if __name__ == "__main__":
     main()
-    print(f(26501365//131))
-
 
